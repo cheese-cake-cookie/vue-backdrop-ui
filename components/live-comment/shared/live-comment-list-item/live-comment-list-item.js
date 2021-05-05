c6166f4d2f624f66c945921afe352d979d2d0836
wasabi <wasabi@podbbang.com>
2021-05-05 15:56:24 +0900

    feat: In process to make live-comment-component

--- components/live-comment/shared/live-comment-list-item/live-comment-list-item.js
+++ components/live-comment/shared/live-comment-list-item/live-comment-list-item.js
@@ -2,29 +2,22 @@ export const LiveCommentListItemComponent = {
   props: {
     comment: {
       type: Object,
-      default: () => {
-        return {
-          id: 1,
-          user: {
-            id: 1,
-            nickname: 'MIA',
-            iamge: 'https://dummyimage.com/400x400/ff36de/ffffff&text=MIA',
-          },
-          content:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, nisi!',
-          createdAt: '2021-05-05 14:27:00',
-        };
-      },
+    },
+  },
+  computed: {
+    userProfileStyle() {
+      return {
+        'background-image': `url('${this.comment.user.image}')`,
+      };
     },
   },
   template: `
     <li class="live-comment-list-item-component">
       <div class="user">
-        <div class="user__profile">
-        </div>
-        <span class="user__nickname">{{ comment.user.nickname }}</span>
+        <div class="user-profile" :style="userProfileStyle"></div>
+        <span class="user-nickname">{{ comment.user.nickname }}</span>
       </div>
-      <span class="created-at">오늘 9:39</span>
+      <span class="created-at">{{ comment.createdAt }}</span>
       <p class="content">{{ comment.content }}</p>
     </li>
   `,
